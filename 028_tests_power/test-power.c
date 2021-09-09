22693b216de178adc1f879c848b39d5eaaeb61e5
zp47 <zp47@duke.edu>
2021-09-08 21:32:54 -0400

    zp47

---
+++ 028_tests_power/test-power.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+unsigned power(unsigned x, unsigned y);
+
+void run_check(unsigned x, unsigned y, unsigned expexted_ans) {
+  unsigned test_value = power(x, y);
+  if (test_value != expexted_ans) {
+    printf("Test: %u to the %u power is %u:\n", x, y, test_value);
+    exit(EXIT_FAILURE);
+  }
+}
+
+int main() {
+  run_check(2, 3, 8);
+  run_check(2, 11, 2048);
+  run_check(1, 1024, 1);
+  run_check(1, 0, 1);
+  run_check(0, 0, 1);
+  run_check(0, 2, 0);
+  run_check(-3, 2, 9);
+  run_check(-3, 3, -27);
+  run_check(2, 33, (unsigned)8589934592);
+  run_check('a', 2, 9409);
+  run_check('a', 1, 'a');
+  run_check('a', 0, 1);
+  run_check((unsigned)-1.5, 0, 1);
+
+  exit(EXIT_SUCCESS);
+}
