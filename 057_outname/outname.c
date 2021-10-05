db1017300e1ca04f1fc4881b489319bfec49b0d3
zp47 <zp47@duke.edu>
2021-10-05 14:14:14 -0400

    057

--- 057_outname/outname.c
+++ 057_outname/outname.c
@@ -1,8 +1,14 @@
+#include "outname.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "outname.h"
 
 char * computeOutputFileName(const char * inputName) {
   //WRITE ME
+  size_t input_len = strlen(inputName);
+  char * answer = malloc((input_len + 8) * sizeof(answer));
+  strcpy(answer, inputName);
+  strcat(answer, ".counts");
+  return answer;
 }
