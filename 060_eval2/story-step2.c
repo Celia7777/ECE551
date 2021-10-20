37dc19b308f19ea905536aff85abb46e828a741f
zp47 <zp47@duke.edu>
2021-10-20 15:25:44 -0400

    060

--- 060_eval2/story-step2.c
+++ 060_eval2/story-step2.c
@@ -4,7 +4,7 @@
 
 int main(int argc, char ** argv) {
   if (argc != 2) {
-    fprintf(stderr, "Usage: input story file");
+    fprintf(stderr, "Usage: input words file");
     return EXIT_FAILURE;
   }
   FILE * f = fopen(argv[1], "r");
@@ -13,22 +13,17 @@ int main(int argc, char ** argv) {
     return EXIT_FAILURE;
   }
   catarray_t * parsewords;
+  //to get the catarray_t with name and words from the words file
+  //which template is category:word
   parsewords = parseCategoryWords(f);
+  //print the catarray_t
   printWords(parsewords);
-  //  cw_pair * checkpair;
-  //char * input = "animal:dog\n";
-  //checkpair = extractCategoryWords(input);
-  //printf("category:%s\n", checkpair->category);
-  //printf("name:%s\n", checkpair->word);
 
   if (fclose(f) != 0) {
     perror("Failed to close the input file!");
     return EXIT_FAILURE;
   }
   freecatarray_t(parsewords);
-  //free(checkpair->category);
-  //free(checkpair->word);
-  //free(checkpair);
 
   return EXIT_SUCCESS;
 }
