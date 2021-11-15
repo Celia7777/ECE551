78f19d0f5f664bcd92cb898c1185471f6bf8c93f
zp47 <zp47@duke.edu>
2021-11-14 22:13:16 -0500

    093

--- 093_eval3/page_backend.cpp
+++ 093_eval3/page_backend.cpp
@@ -1,5 +1,4 @@
 #include <algorithm>
-#include <cctype>
 #include <cstdio>
 #include <cstdlib>
 #include <fstream>
@@ -11,40 +10,8 @@ class Page {
  private:
   std::vector<std::string> navigation;
   std::vector<std::string> text;
-  std::vector<int> pagechoices;
 
  public:
-  void readOnepage(std::istream & f) {
-    std::string line;
-    //record the index of # in a line
-    std::string::size_type pound_pos;
-    //the line number which is #
-    size_t pound_num = 0;
-    bool haspound = false;
-    //    size_t line_index = 0;
-    //bool checknav = false;
-
-    while (std::getline(f, line)) {
-      pound_pos = line.find("#");
-      //  std::cout << position << "\n";
-      if (pound_num == 0 && pound_pos == 0) {
-        std::cerr << "no navigation part, only a #\n";
-        exit(EXIT_FAILURE);
-      }
-
-      //find the valid #
-      if (pound_num != 0 && pound_pos == 0) {
-        haspound = true;
-        break;
-      }
-      pound_num++;
-    }
-    if (haspound == false) {
-      std::cerr << "we cannot find a valid #\n";
-      exit(EXIT_FAILURE);
-    }
-  }
-
   //a method to parse the input story and check its #
   //if the # meets the requirements of the input format
   //return the line number of the #
@@ -83,10 +50,8 @@ class Page {
   //check if it is WIN, LOSE or "integer:text"
   //if it is, return true; if not, return false
   bool checkNavigation(std::string & s) {
-    std::string intstr;
+    std::string integer;
     std::string::size_type colonpos;
-    std::string::size_type sz;
-    int integer;
     colonpos = s.find(":");
     //if the choice is WIN or CLOSE
     if (s.compare("WIN") == 0 || s.compare("LOSE") == 0) {
@@ -94,26 +59,15 @@ class Page {
       return true;
     }
     else {
-      //if the choice does not have colon,
+      //if the choice does not have :
       //or the : at first, no integer
       if (colonpos == s.npos || colonpos == 0) {
-        std::cerr << "no colon or at the colon is at the first, thus no integer part\n";
+        std::cout << "no colon or at the first\n";
         //std::cout << colonpos << "\n";
-        return false;  //应该是return，还是exit
+        return false;
       }
-      //check if the integer part is valid or not
       else {
-        intstr = s.substr(0, colonpos);
-        for (size_t i = 0; i < colonpos; i++) {
-          if (std::isdigit(intstr[i]) == 0) {
-            std::cerr << "the part before colon is not invalid integer\n";
-            return false;
-          }
-          integer = std::stoi(intstr, &sz);
-          pagechoices.push_back(integer);
-          //std::cout<<"int choice:"<<pagechoices[]
-        }
-
+        integer = s.substr(0, colonpos);
         //std::cout << "integer\n";
         return true;
         //tell if it is an integer later
@@ -121,9 +75,6 @@ class Page {
     }
   }
 
-  //firstly check the # and the navigation part,
-  //if they are both valid, push the navigation and text
-  //into the corresponding vector structure
   void pushStory(std::istream & f) {
     std::string line;
     size_t line_num;
@@ -131,10 +82,11 @@ class Page {
     bool checknav = false;
     line_num = findPound(f);
     //std::cout << "# at line: " << line_num << "\n";
-    //rewind the file stream pointer to the beginning
     f.seekg(0, f.beg);
+    //while (!f.eof()) {
     while (std::getline(f, line)) {
       //std::cout << line << "-\n";
+
       if (line_index < line_num) {
         checknav = checkNavigation(line);
         if (checknav == false) {
@@ -142,16 +94,18 @@ class Page {
           exit(EXIT_FAILURE);
         }
         navigation.push_back(line);
+        //        std::cout << "nav:" << navigation[line_index];
       }
 
       if (line_index > line_num) {
         text.push_back(line);
+        //std::cout << "nav:" << text[line_index];
       }
+
       line_index++;
     }
   }
 
-  //print one page of the story
   void printStory() {
     std::string::size_type colonindx;
     std::string choiceinfo;
@@ -174,48 +128,4 @@ class Page {
       }
     }
   }
-
-  //verify each page, when this function is called
-  //the page class is already created and the navigation
-  //part has already checked valid
-  bool verifyPage(int pagenum, int & win, int & lose, std::vector<int> & intvec) {
-    std::cout << "page:" << pagenum << std::endl;
-    //int winnum = 0;
-    //int losenum = 0;
-    std::string::size_type colonpos;
-    std::string::size_type sz;
-    std::string intstr;
-    int integer;
-    //std::vector<int> intvector;
-
-    for (size_t i = 0; i < navigation.size(); i++) {
-      //std::cout << "nav:" << navigation[i] << std::endl;
-      if (navigation[i].compare("WIN") == 0) {
-        win++;
-        //        std::cout << "winwinwin" << std::endl;
-      }
-      else if (navigation[i].compare("LOSE") == 0) {
-        lose++;
-      }
-      else {
-        colonpos = navigation[i].find("#");
-        intstr = navigation[i].substr(0, colonpos);
-        integer = std::stoi(intstr, &sz);
-        if (integer == 0 || integer > pagenum) {
-          std::cerr << "the page referenced is invalid" << std::endl;
-          exit(EXIT_FAILURE);
-        }
-        else {
-          intvec[integer - 1]++;
-        }
-      }
-      //std::cout << "integer:" << intvec[i] << std::endl;
-    }
-
-    return true;
-  }
-
-  std::vector<std::string> getNavigation() { return navigation; }
-  std::vector<int> getChoices() { return pagechoices; }
-  size_t getNavsize() { return navigation.size(); }
 };
---
+++ 093_eval3/story.cpp
@@ -0,0 +1,110 @@
+#include <algorithm>
+#include <cctype>
+#include <cstdio>
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <istream>
+#include <vector>
+
+#include "page.cpp"
+
+class Story {
+ private:
+  std::vector<Page> story;
+
+ public:
+  //read the directory and check its filename
+  //return the number of files
+  int readInputstory(char * directory) {
+    int pagenum = 1;
+    // std::ifstream f;
+
+    while (true) {
+      Page page;
+      std::ifstream f;
+      std::string filename(directory);
+      //std::cout << filename << std::endl;
+      filename.append("/page");
+      //std::cout << filename << std::endl;
+      filename.append(std::to_string(pagenum));
+      //std::cout << filename << std::endl;
+      filename.append(".txt");
+      //std::cout << filename << std::endl;
+
+      f.open(filename);
+      //if (f.fail()) {
+      // std::cerr << "failed to open this file" << std::endl;
+      // exit(EXIT_FAILURE);
+      //}
+      //have wrong page number (not continuous) when read file
+      //so a page dose not exist, we need stop read page files
+
+      if (!f.good()) {
+        //        std::cout << "here!" << std::endl;
+        break;  //???
+      }
+      page.pushStory(f);
+      page.printStory();
+      story.push_back(page);
+
+      pagenum++;
+    }
+
+    //no page1.txt
+    if (story.size() == 0) {
+      std::cerr << "no page1.txt" << std::endl;
+      exit(EXIT_FAILURE);
+    }
+    return pagenum - 1;
+  }
+
+  void printStory() {
+    for (size_t i = 0; i < story.size(); i++) {
+      story[i].printStory();
+    }
+  }
+
+  size_t getsize() { return story.size(); }
+  std::vector<Page> getstory() { return story; }
+
+  void createStory(int pagenum) {
+    int integer;
+    int storypg = 0;
+    int movepage;
+    story[0].printStory();
+    while (true) {
+      //check if WIN or LOSE page
+      if (story[storypg].getNavigation()[0].compare("WIN") == 0 ||
+          story[storypg].getNavigation()[0].compare("LOSE") == 0) {
+        exit(EXIT_SUCCESS);
+      }
+
+      else {
+        while (true) {
+          std::cin >> integer;
+          if (!std::cin.good()) {
+            std::cerr << "Please input an valid integer" << std::endl;
+            continue;
+          }
+          if (integer <= 0 || integer > (int)story[storypg].getNavsize()) {
+            std::cerr << "Please input an integer between 0 and "
+                      << story[storypg].getNavsize() << std::endl;
+            continue;
+          }
+          break;
+        }
+      }
+      movepage = story[storypg].getChoices()[integer - 1];
+      std::cout << "move page" << movepage << std::endl;
+      story[movepage - 1].printStory();
+      //for (size_t j = 0; j < story[i].getNavsize(); j++) {
+      //check if WIN or LOSE page
+      //if (story[i].getNavigation()[j].compare("WIN") == 0 ||
+      //  story[i].getNavigation()[j].compare("LOSE") == 0) {
+      //  exit(EXIT_SUCCESS);
+      //}
+      storypg = movepage - 1;
+    }
+  }
+};
