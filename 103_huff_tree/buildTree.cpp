c6e3523049b389a59f393580b3654854c3940cd8
zp47 <zp47@duke.edu>
2021-11-16 00:13:52 -0500

    103

--- 103_huff_tree/buildTree.cpp
+++ 103_huff_tree/buildTree.cpp
@@ -1,5 +1,25 @@
 #include "node.h"
 
-Node *  buildTree(uint64_t * counts) {
+Node * buildTree(uint64_t * counts) {
   //WRITE ME!
-}  
+
+  priority_queue_t pq;
+  for (size_t i = 0; i < 257; i++) {
+    if (counts[i] != 0) {
+      pq.push(new Node(i, counts[i]));
+    }
+  }
+  if (pq.empty()) {
+    return NULL;
+  }
+
+  while (pq.size() > 1) {
+    Node * l = pq.top();
+    pq.pop();
+    Node * r = pq.top();
+    pq.pop();
+    Node * tree = new Node(l, r);
+    pq.push(tree);
+  }
+  return pq.top();
+}
